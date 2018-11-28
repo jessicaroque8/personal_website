0ce2247c3a81bfef16a7e81c21cdd5bf26bd2a65
Jessica Roque <jroque.dev@gmail.com>
2018-11-27 20:41:22 -0800

    feat: add link to github repo in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,11 @@
 import { h } from 'preact';
-import style from './style';
+import LinkWrapper from '../link-wrapper';
 
 const Footer = () => (
 	<footer>
-		<p>Made with xo using Preact.</p>
+		<p>
+			Made with xo using Preact. <LinkWrapper href="https://github.com/jessicaroque8/personal_website">See it on GitHub.</LinkWrapper>
+		</p>
 	</footer>
 );
 export default Footer;
