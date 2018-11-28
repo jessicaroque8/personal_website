2c6eccc61f3bf74b6908dca0d5dc03b8cff49fbd
Jessica Roque <jroque.dev@gmail.com>
2018-11-27 20:52:35 -0800

    style: add more padding to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,12 @@
 import { h } from 'preact';
+import style from './style';
 import LinkWrapper from '../link-wrapper';
 
 const Footer = () => (
 	<footer>
 		<p>
-			Made with xo using Preact. <LinkWrapper href="https://github.com/jessicaroque8/personal_website">See it on GitHub.</LinkWrapper>
+			Made with xo using Preact.{' '}
+			<LinkWrapper href="https://github.com/jessicaroque8/personal_website">Check out the code on GitHub.</LinkWrapper>
 		</p>
 	</footer>
 );
